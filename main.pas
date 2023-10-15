9cab6ef79d66f201784675d0cdf9e7a783cdd682
Ygor H <ygorhenrrique360@gmail.com>
2023-10-15 11:11:37 -0300

    pri comit

---
+++ main.pas
@@ -0,0 +1,26 @@
+program Hatd;
+uses crt,Graph,unihat ;
+
+
+procedure versao();
+Begin
+//rewrite()
+
+//reset()
+
+
+end;
+
+begin
+  
+  
+initgraph(0,0,'');
+
+
+
+menu();
+
+
+
+  readln;
+end.
